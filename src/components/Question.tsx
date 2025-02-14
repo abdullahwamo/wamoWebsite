f8fea600616f2ff67147ae7192713e188095f91f
Abdullah <abdullah.zia@wamolabs.com>
2025-02-15 03:48:41 +0500

    style

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -5,9 +5,12 @@ const Question = () => {
   return (
     <div className='flex flex-col items-center rounded-xl justify-center mt-2 mx-auto my-24 bg-white dark:bg-[#1e1e1e] w-[80%] h-[200px] text-black dark:text-white'>
       <div className=''>
-        <div className='text-3xl my-2 px-4 items-center justify-center w-full '>Do you have any question for us?</div>
-        <div className='flex flex-row items-center justify-center w-full'>
-          <span className='text-2xl '>Reach us Out {'->'}</span>
+        <div className='text-3xl my-2 px-8 md:px-4 items-center justify-center w-full '>
+          Do you have any question for us?
+        </div>
+        <div className='flex flex-col md:flex-row items-center justify-center w-full'>
+          <span className='text-2xl my-2 md:my-0 '>Reach us Out </span>
+          <span className='hidden md:visible'>{'->'}</span>
           <Link className='ml-2' href='/contact'>
             <div>
               <button className='bg-orange-500 rounded-lg p-2 text-white'>Contact Us</button>
--- src/components/services/components/Hero.tsx
+++ src/components/services/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
         <div className='relative bg-transparent inset-0 bg-black bg-opacity-50 flex'>
           <div className='w-full text-center px-6 mx-24 my-48 '>
             <h1 className='text-orange-500 text-3xl font-bold w-full'>The key to Solutions</h1>
-            <p className='text-white mt-4 w-full'>
+            <p className='text-white mt-4 w-full break-words'>
               Get a chance to work with a global network of 800+ members, with offices in Malaysia, USA, Australia,
               Germany, and Pakistan. Using cutting-edge technologies, we help businesses move one step closer to digital
               transformation.
