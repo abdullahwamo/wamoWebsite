7ac74e9a143f8f2c4e7562cb9dd25a12794638da
Abdullah <abdullah.zia@wamolabs.com>
2025-02-17 16:56:39 +0500

    changes to ui

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -6,7 +6,7 @@ const PortfolioIndex = () => {
   return (
     <div className='w-full h-full bg-white dark:bg-[#121212] text-black dark:text-white'>
       <Portfolio />
-      <div className='bg-white dark:bg-[#121212] h-[200px]'>
+      <div className='bg-white dark:bg-[#121212] h-[200px] mt-8 md:mt-10'>
         <Question />
       </div>
     </div>
